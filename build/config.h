6ceb551d3303062aa17a2299f4e5595298adb630
VKziado <66130208+VKziado@users.noreply.github.com>
2023-08-18 16:29:49 +0100

    remove build

--- build/config.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#define WORKING_DIR "E:/MSC-Project/simpleopengl/build/bin"
-#define PROJECT_DIR "E:/MSC-Project/simpleopengl"
-#define SHADER_DIR "E:/MSC-Project/simpleopengl/assets/shaders"
-#define ASSETS_DIR "E:/MSC-Project/simpleopengl/assets"
